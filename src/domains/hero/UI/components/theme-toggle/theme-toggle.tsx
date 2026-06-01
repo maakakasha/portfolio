5b0545721a3c91930a615b6ecf223298c909bb80
maakakasha <maakakasha1@gmail.com>
2026-06-01 22:34:31 +0400

    Added dark mode as default

--- src/domains/hero/UI/components/theme-toggle/theme-toggle.tsx
+++ src/domains/hero/UI/components/theme-toggle/theme-toggle.tsx
@@ -3,7 +3,7 @@ import "./theme-toggle.css";
 export function ThemeToggle() {
   return (
     <>
-      <input type="checkbox" id="theme-toggle" className="theme-toggle-input" defaultChecked />
+      <input type="checkbox" id="theme-toggle" className="theme-toggle-input" />
       <label htmlFor="theme-toggle" className="theme-toggle" aria-label="Toggle theme" />
     </>
   );
